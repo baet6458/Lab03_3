33ab30503c42f16df821f4ae348ea95176371b27
Elijsha Baetiong <baet6458@kettering.edu>
2019-05-02 02:08:37 -0400

    Added Team A and B java and xml point tracker

--- app/src/main/java/android/example/com/MainActivity.java
+++ app/src/main/java/android/example/com/MainActivity.java
@@ -2,20 +2,61 @@ package android.example.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA=0;
+    int scoreTeamB=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
     }
+    public void addThreeForTeamA(View v){
+        scoreTeamA=scoreTeamA+3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View v){
+        scoreTeamA=scoreTeamA+2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addoneForTeamA(View v){
+        scoreTeamA=scoreTeamA+1;
+        displayForTeamA(scoreTeamA);
+    }
+    public void addThreeForTeamB(View v){
+        scoreTeamB=scoreTeamB+3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View v){
+        scoreTeamB=scoreTeamB+2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addoneForTeamB(View v){
+        scoreTeamB=scoreTeamB+1;
+        displayForTeamB(scoreTeamB);
+    }
+
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
