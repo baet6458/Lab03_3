16fe610061b917a2efa7f4e5829e2b77d25808f5
Elijsha Baetiong <baet6458@kettering.edu>
2019-05-02 01:48:26 -0400

    adding more stuff

--- app/src/main/java/android/example/com/MainActivity.java
+++ app/src/main/java/android/example/com/MainActivity.java
@@ -10,16 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int weekday = 5;
-        int weekend = 9;
-        int optimalHours = 7 * 8;
-        int actualHours = weekday;
-        actualHours = actualHours + weekend * 2;
-        int solution = optimalHours - actualHours;
-        display(solution);
     }
-    public void display (int i) {
-        TextView t = (TextView) findViewById(R.id.display_text_view);
-        t.setText(""+i);
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
